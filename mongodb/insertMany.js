f370f36c60d8a8f047dec1df0d4cb584bfc115a7
Yuriko Goto <gotoyuriko95@gmail.com>
2023-12-01 21:22:04 +0900

    Add mongo db

---
+++ mongodb/insertMany.js
@@ -0,0 +1,28 @@
+const { MongoClient } = require('mongodb');
+const uri = "**********";
+const client = new MongoClient(uri);
+
+async function run() {
+    const database = client.db('notes');
+    const notes = database.collection('notes');
+
+    // Create Data
+    const query = [{
+        id: 1,
+        title: 'ノート１のタイトルです',
+        subTitle: 'ノート１のサブタイトルです',
+        bodyText: 'ノート１の本文です'
+    },
+    {
+        id: 2,
+        title: 'ノート２のタイトルです',
+        subTitle: 'ノート２のサブタイトルです',
+        bodyText: 'ノート２の本文です'
+    }];
+
+    const note = await notes.insertMany(query);
+    console.log(note);
+    await client.close();
+}
+
+run();
