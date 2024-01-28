0c00d8fbf6ff99c7a6768583865c7fd56b7fb340
Yuriko Goto <gotoyuriko95@gmail.com>
2024-01-28 23:01:15 +0800

    add changes

--- app.js
+++ app.js
@@ -45,7 +45,11 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    title: 'Error',
+    message: err.message,
+    error: err,
+  });
 });
 
 module.exports = app;
